64540179a50ce9c0d41e1ae9d7dd3f6e2512a6e1
echo.xie <echo.xie@cootek.cn>
2018-05-09 20:18:55 +0800

    修改选择item的背景

--- app/src/main/java/com/meiyin/moneyrecorder/activities/RecordActivity.java
+++ app/src/main/java/com/meiyin/moneyrecorder/activities/RecordActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.meiyin.moneyrecorder.Fragments.IncomeFragment;
 import com.meiyin.moneyrecorder.Fragments.PayFragment;
@@ -16,10 +17,14 @@ import com.meiyin.moneyrecorder.R;
 public class RecordActivity extends FragmentActivity {
     IncomeFragment incomeFragment;
     PayFragment payFragment;
+    TextView income_tv;
+    TextView pay_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        income_tv = (TextView) findViewById(R.id.income_tv);
+        pay_tv = (TextView) findViewById(R.id.pay_tv);
         initUI();
         bindEvents();
     }
@@ -31,25 +36,34 @@ public class RecordActivity extends FragmentActivity {
     }
 
     private void bindEvents() {
-        findViewById(R.id.pay_tv).setOnClickListener(new View.OnClickListener() {
+        pay_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showPayFragment();
+                pay_tv.setBackgroundColor(RecordActivity.this.getResources().getColor(R.color.record_type_button_selected));
+                income_tv.setBackgroundColor(RecordActivity.this.getResources().getColor(R.color.record_type_button_unselected));
             }
         });
-        findViewById(R.id.income_tv).setOnClickListener(new View.OnClickListener() {
+        income_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                if (payFragment.isAdded()) {
-                    transaction.remove(payFragment);
-                }
-                if (!incomeFragment.isAdded()) {
-                    transaction.replace(R.id.record_fragment, incomeFragment);
-                }
-                transaction.commit();
+                showIncomeFragment();
+                pay_tv.setBackgroundColor(RecordActivity.this.getResources().getColor(R.color.record_type_button_unselected));
+                income_tv.setBackgroundColor(RecordActivity.this.getResources().getColor(R.color.record_type_button_selected));
             }
         });
+    }
+
+    private void showIncomeFragment() {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        if (payFragment.isAdded()) {
+            transaction.remove(payFragment);
+        }
+        if (!incomeFragment.isAdded()) {
+            transaction.replace(R.id.record_fragment, incomeFragment);
+        }
+        transaction.commit();
+
     }
 
     private void showPayFragment() {
